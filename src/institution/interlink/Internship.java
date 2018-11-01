48c999fd1bdca20d7a22af10f94ae8e64f1f4014
Alex Akulenko <oleksandr.akulenko0@gmail.com>
2018-11-01 18:31:28 +0200

    The setStudent method in Internship is implemented

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -1,16 +1,24 @@
 package institution.interlink;
 
+import institution.University;
 import person.Student;
+
+import java.util.ArrayList;
 
 public class Internship {
     private String name;
+    private ArrayList<Student> students = new ArrayList<>();
+    private ArrayList<University> universities = new ArrayList<>();
 
     public Internship(String name) {
         this.name = name;
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public void setStudent(Student student, University university) {
+        if ( !(universities.contains(university)) ) {
+            universities.add(university);
+        }
+        students.add(student);
     }
 
     public String getStudents() {
